25bdb8dc41a26c1cf1cac6c572815ccf8a834ff7
Tim Lim <Tim Lim>
2015-12-05 17:06:41 -0500

    some cleaning up

--- graph.py
+++ graph.py
@@ -16,46 +16,41 @@ def print_edge_info(G, node_pairs):
     given list of node pairs, print date and data info of all edges shared between them.
     """
     for (n1, n2) in node_pairs:
-        print('For nodes: ' + str(n1) + ', ' + str(n2))
+        print 'For nodes {}, {}:'.format(n1,n2)
         edge_dict = G[n1][n2]
         for key in edge_dict:
             attributes = edge_dict[key]
             # assume we have date (string), data (list of strings)
-            print("\tdate : " + attributes['date'])
-            print("\tdata: ")
+            print "\tdate : {}".format(attributes['date'])
+            print "\tdata: "
             for thing in attributes['data']:
-                print("\t\t" + thing)
+                print "\t\t{}".format(thing)
+        print ''
 
-#the [20::] can walys be changed to some other amount!
-def eigCent(G):
+
+def eig_cent(G):
     centrality = nx.eigenvector_centrality_numpy(G)
-    #print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
-    return centrality[-20:]
+    centrality = relevant_cen_results(centrality)
 
 
-def degCent(G):
+def deg_cent(G):
     centrality = nx.degree_centrality(G)
-    #print(['%s %0.2f'%(node,centrality[node]) for node in centrality])
-    return centrality[:20]
-    
-def hubCent(G):
-    cent = nx.hits(G)
-    #cent[0] =  hubs; cent[1] = authorities
-    return cent[-20:]
-    
+    return relevant_cen_results(centrality)
 
-def releventCenResults(dic1):            
-    dic2 = {key:dic1[key] for key in dic1 if not re.search(r'\d', key)}                
-    return dic2                
+def hub_cent(G):
+    centrality = nx.hits(G)
+    centrality = relevant_cen_results(centrality)
 
-# infile = 'makegraph/sampleoutput.txt'
-# num_lines = 100
-# edgelist = parseToGraph.parse(infile, num_lines)
+
+def relevant_cen_results(dic1):
+    dic2 = {key:dic1[key] for key in dic1 if not re.search(r'[a-z]', key) is None}
+    return dic2
+
+
+# infile = '2013-03-17'
+# num_edges = 10
+# edgelist = parseToGraph.parse(infile, num_edges=num_edges)
 # G = construct_graph(edgelist)
 # print_edge_info(G, G.edges())
-
-
-
-
-
-
+# centrality = deg_cent(G)
+# print centrality
