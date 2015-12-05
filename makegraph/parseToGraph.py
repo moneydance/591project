a1a670f70a4b04840f7a1974ffc98b1310297cac
Benajamin Pusey <bpusey@bu.edu>
2015-12-05 16:10:25 -0500

    edge parser works

--- makegraph/parseToGraph.py
+++ makegraph/parseToGraph.py
@@ -10,7 +10,7 @@ $ webb.bin SOA redacted"
 """
 
 DATA_FLAGS = ['?', '$', '!']
-SEPCOUNTMAX = 2
+SEPCOUNTMAX = 1
 RESPONSE = 'r'
 ADDRESSLOOKUP = 'A'
 
@@ -24,27 +24,26 @@ class DnsEdge():
         self.data = []
 
     def parse_line(self, line):
-        if line == "<SEP>":
-            print("sep line")
-            self.sepcount += 1
-        elif self.valid:     # If not valid then just read lines to next SEP
-            starting_char = line[0]
-            if starting_char in DATA_FLAGS:  # line contains Data
-                self.data += [line]
-                if starting_char == '?':
-                    components = line.split()
-                    if components[2] == ADDRESSLOOKUP:
-                        self.external = components[1]
+        if line:
+            if line == "<SEP>":
+                self.sepcount += 1
+            elif self.valid:     # If not valid then just read lines to next SEP
+                starting_char = line[0]
+                if starting_char in DATA_FLAGS:  # line contains Data
+                    self.data += [line]
+                    if starting_char == '?':
+                        components = line.split()
+                        if components[2] == ADDRESSLOOKUP:
+                            self.external = components[1]
+                        else:
+                            self.valid = False
+                else:
+                    components = [x.strip() for x in line.split(',')]
+                    if components[5] == RESPONSE:
+                        self.date = components[0]
+                        self.internal = components[2]
                     else:
                         self.valid = False
-            else:
-                components = [x.strip() for x in line.split(',')]
-                if components[5] == RESPONSE:
-                    self.date = components[0]
-                    self.internal = [2]
-                else:
-                    self.valid = False
-
 
 
 def parse(infile, num_lines):
@@ -52,10 +51,9 @@ def parse(infile, num_lines):
     with open(infile, "r") as f:
         edge = DnsEdge()
         for i in range(num_lines):
-            line = f.readline()
+            line = f.readline().rstrip()
             edge.parse_line(line)
             if edge.sepcount == SEPCOUNTMAX:
-                print('this happened')
                 if edge.valid:
                     edgelist += [edge]
                 edge = DnsEdge()
@@ -65,6 +63,6 @@ def parse(infile, num_lines):
 infile = 'sampleoutput.txt'
 num_lines = 100
 edgelist = parse(infile, num_lines)
-print(edgelist)
+print(edgelist[1].data)
 
 
